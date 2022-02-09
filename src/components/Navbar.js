0f793f0a5b3f58d2930f028df848883d2d35fad4
anjal-i <anjaliahuja99@yahoo.com>
2022-02-09 18:20:32 +1100

    add button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { RiHeartsLine } from "react-icons/ri";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <div className="navbar">
@@ -13,7 +18,36 @@ function Navbar() {
             <RiHeartsLine className="navbar-icon" />
             Date Ideas
           </Link>
-          <div className="menu-icon"></div>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links">
+                About
+              </Link>
+            </li>
+
+            <li className="nav-btn">
+              {button ? (
+                <Link to="/contact" className="btn-link">
+                  <Button buttonStyle="btn--outline"></Button>
+                </Link>
+              ) : (
+                <Link to="/contact" className="btn-link">
+                  <Button
+                    buttonStyle="btn--outline"
+                    buttonSize="btn--mobile"
+                  ></Button>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
