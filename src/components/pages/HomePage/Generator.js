7cc1970e19f1dc90fbdb17239d5ff21d488062a2
anjal-i <anjaliahuja99@yahoo.com>
2022-02-16 17:31:17 +1100

    add and center generate button

--- src/components/pages/HomePage/Generator.js
+++ src/components/pages/HomePage/Generator.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../Button";
+import "./Generator.css";
 
 function Generator() {
+  const [click, setClick] = useState(false);
+  const generate = () => setClick(true);
+
   return (
-    <div className="generator-section light">
-      <div className="row generator-row">
-        <div className="col">
-          <Button buttonSize="btn--xlarge">Generate</Button>
+    <div className="generator-section">
+      {" "}
+      <div className="container">
+        <div
+          className="row generator-row"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div className="col">
+            <div className="btn-wrapper">
+              <Button
+                buttonStyle="btn--primary"
+                buttonSize="btn--xlarge"
+                onClick={generate}
+              >
+                Generate
+              </Button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="top-line">You should... </div>
+          </div>
         </div>
-        <div className="col">You should... </div>
       </div>
     </div>
   );
