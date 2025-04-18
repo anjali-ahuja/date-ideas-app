b293ebd43be102473dafedbed83ca3ff0870b3d0
Anjali <anjali.manoj.ahuja@gmail.com>
2025-04-18 22:12:58 +1000

    update contact page text

--- src/components/pages/ContactPage/ContactData.js
+++ src/components/pages/ContactPage/ContactData.js
@@ -5,7 +5,7 @@ export const contactObjOne = {
   topLine: "About the author",
   headline: "Hi, I am Anjali.",
   description:
-    "I love all things tech. The areas I am skilled in are Software Engineering, Machine Learning, Data Modelling and Cloud Solutioning. I wish I had a greater purpose in life but I don't. Till I find it, I just want to savor life, and be good.",
+    "I love all things tech. The areas I am skilled in are Software Engineering, Machine Learning, Data Modelling and Cloud Solutioning. I love learning new skills and building things.",
   buttonLabel: "",
   imgStart: "",
   img: "./images/me.JPG",
